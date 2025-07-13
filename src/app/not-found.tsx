cccc0cf8db9b0a55b3ac164e06dd5506e40f08c0
Sihara17 <147967440+Sihara17@users.noreply.github.com>
2025-07-13 22:06:58 +0700

    Update not-found.tsx

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,10 +1,8 @@
-// app/not-found.tsx
-
+// src/app/not-found.tsx
 export default function NotFound() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-black text-white flex-col">
-      <h1 className="text-4xl font-bold">404 - Page Not Found</h1>
-      <p className="mt-2 text-gray-400">Sorry, the page you're looking for doesn't exist.</p>
+    <div className="h-screen flex items-center justify-center">
+      <h1 className="text-2xl font-bold">404 - Page Not Found</h1>
     </div>
-  );
+  )
 }
