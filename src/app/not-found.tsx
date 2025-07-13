efaaaf49dc7f75f8cb73d1d8d321d85be44bef45
Sihara17 <147967440+Sihara17@users.noreply.github.com>
2025-07-13 22:10:05 +0700

    Update not-found.tsx

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,8 +1,11 @@
 // src/app/not-found.tsx
 export default function NotFound() {
   return (
-    <div className="h-screen flex items-center justify-center">
-      <h1 className="text-2xl font-bold">404 - Page Not Found</h1>
+    <div className="flex items-center justify-center min-h-screen bg-black text-white">
+      <div className="text-center px-4">
+        <h1 className="text-4xl font-bold mb-4">404 - Page Not Found</h1>
+        <p className="text-gray-400">Maaf, halaman yang kamu cari tidak ditemukan.</p>
+      </div>
     </div>
   )
 }
